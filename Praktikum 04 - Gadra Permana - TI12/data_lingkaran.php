285dda7e5d94959d570590aaedee7e4d05cc1718
Gadra Permana <91862698+ohmygad9@users.noreply.github.com>
2022-03-27 23:19:09 +0700

    Add files via upload

---
+++ Praktikum 04 - Gadra Permana - TI12/data_lingkaran.php
@@ -0,0 +1,13 @@
+<?php
+    require_once "class_lingkaran.php";
+    
+   
+    $lingkar1 = new Lingkaran( 10 );
+  
+    echo '---------LINGKARAN---------';
+    echo '<br>';
+    echo "<br>Nilai PHI: " . Lingkaran::PHI;
+    echo "<br>Jari-jari: ".' '. $lingkar1->jari.'cm';
+    echo "<br>Keliling: ".$lingkar1->getKeliling().'cm';
+    echo "<br>Luas: ".' '. $lingkar1->getLuas().'cm²';
+?>
